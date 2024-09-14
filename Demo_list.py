25af304e5cd704080bcfab04a27f45e84d396c4b
Kyan Cuarte <kyancuarte22@yahoo.com>
2024-09-14 11:23:37 +0800

    First Recitation

---
+++ Demo_list.py
@@ -0,0 +1,19 @@
+#Create a List
+
+animals = ["dog", "cow", "sheep", "cat", "rat", "hamster"]
+
+#Print List
+
+print (animals) 
+
+
+#Print Specifics
+
+print (animals[4])
+
+
+#Remove Variable
+
+del (animals[3])
+
+print (animals)
